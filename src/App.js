617a0d761f9724f3c48c471a20e99b6dc63487dd
elham <elhamrouzban778@yahoo.com>
2022-01-25 20:15:30 +0100

    added bootstrap, react-bootstrap, fontawesome

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
-// import { Alert, Button, Badge } from "react-bootstrap";
+ import { Alert, Button, Badge } from "react-bootstrap";
 // import { ToastContainer, toast } from "react-toastify";
 // import SimpleContext from "../../personmanager/src/contex/simpleContex";
+
+import Header from "./components/Header";
+
 
 const App = () => {
     // const [getPersons, setPersons] = useState([]);
@@ -12,20 +15,70 @@ const App = () => {
         // <SimpleContext.Provider
         //     value={{
         //     }}>
-            <div>
-                <form>
-                    <label>Enter your name:
+        <div className="container">
+            <div className="card" >
+                <div className="card-header">
+                    <Header />
+                </div>
+                <div className="card-body">
+                    <div>
+                        <label className="">Card Value
+                        </label>
                         <input
-                            type="text"
-                            // value={name}
-                            // onChange={(e) => setName(e.target.value)}
-                        />
-                    </label>
-                </form>
+                                type="number"
+                                name="age"
+                            // value={inputs.age || ""}
+                            // onChange={handleChange}
+                            />
+
+                    </div>
+                    <div>
+                        <label>Delivery Distance
+                        </label>
+                        <input
+                                type="number"
+                                name="age"
+                            // value={inputs.age || ""}
+                            // onChange={handleChange}
+                            />
+
+                    </div>
+                    <div>
+                        <label>EAmount of items
+                        </label>
+                        <input
+                                type="number"
+                                name="age"
+                            // value={inputs.age || ""}
+                            // onChange={handleChange}
+                            />
+
+                    </div>
+                    <div>
+                        <label>Time
+                        </label>
+                        <input
+                                type="date"
+                                name="age"
+                            // value={inputs.age || ""}
+                            // onChange={handleChange}
+                            />
+
+                    </div>
+
+
+
+                </div>
+                <div className="card-footer"></div>
+
             </div>
+
+
+            <Button  />
+        </div>
         // </SimpleContext.Provider>
 
 
     )
-        }
-    export default App;
+}
+export default App;
