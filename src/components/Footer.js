0e91653d9c1150b48e5cea6be718bb24837fc4e7
elham <elhamrouzban778@yahoo.com>
2022-01-25 20:35:48 +0100

    added Fotter component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Alert } from "react-bootstrap";
+
+
+const Footer = () => {
+    return ( 
+        <div className="">
+<Alert variant="info" className="">....</Alert>
+        </div>
+     );
+}
+ 
+export default Footer;
