4aefa1bb393e8f5b0f354efa101fe378785487ca
elham <elhamrouzban778@yahoo.com>
2022-01-25 20:36:39 +0100

    set body with input

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import Input from './Input';
+
 
 const Body = () => {
     return ( 
         <div>
-            
+            <Input lable="Card Value " type="number"/>
+            <Input lable="Delivery Distance " type="number"/>
+            <Input lable="EAmount of items " type="number"/>
+            <Input lable="Time" type="date"/>
+
         </div>
      );
 }
