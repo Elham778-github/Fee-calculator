0b0d3a4f8885195a876f57994258210c3db137c8
elham <elhamrouzban778@yahoo.com>
2022-01-25 20:14:51 +0100

    added body, header , input component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Alert } from "react-bootstrap";
+
+const Header = () => {
+    return (
+        <div>
+            <Alert variant="info" >Deliver Fee Calculator
+            </Alert>
+
+        </div>
+    );
+}
+
+export default Header;
